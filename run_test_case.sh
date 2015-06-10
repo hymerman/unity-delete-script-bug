85fd1bca79fd47ab9cdb5be7e915e4496fd5f74e
Dave <->
2015-06-10 15:52:03 +0100

    found minimal repro... 4 meta files though

--- run_test_case.sh
+++ run_test_case.sh
@@ -11,13 +11,13 @@ OUTPUT_DIR=$(pwd)/_output/
 # Start clean
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
-rm -rf snapshota snapshotb fail-meta
+rm -rf snapshota snapshotb snapshotc fail-meta
 
 echo "1. Run first Unity build ... this should complete successfully"
 $UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -outputDir=$OUTPUT_DIR/build_step_1/
 echo "      Result: " $?
+sleep 2
 cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_1.log
-
 
 echo "2. Remove a script file and it's corresponding meta file"
 mkdir -p $(pwd)/tmp_deleted_file/
@@ -30,11 +30,11 @@ cp snapshota/Library/metadata/eb/ebf102192028942cdb156ff88fe33d6a      fail-meta
 cp snapshota/Library/metadata/eb/ebf102192028942cdb156ff88fe33d6a.info fail-meta/
 cp snapshota/Library/assetDatabase3                                    fail-meta/
 cp snapshota/Library/guidmapper                                        fail-meta/
-cp snapshota/Library/metadata/00/00000000000000004000000000000000      fail-meta/
 
 echo "3. Run Unity build again ... this should fail"
 $UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -outputDir=$OUTPUT_DIR/build_step_3/
 echo "      Result: " $?
+sleep 2
 cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_3.log
 
 echo "3.1 save snapshot b"
@@ -43,14 +43,14 @@ cp -r $UNITY_PROJECT_PATH snapshotb
 echo "4. Run Unity build again ... this should complete successfully"
 $UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -outputDir=$OUTPUT_DIR/build_step_4/
 echo "      Result: " $?
+sleep 2
 cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_4.log
 
-echo "4.1 DONT insert meta files which seemed to cause failiure:"
-#cp fail-meta/ebf102192028942cdb156ff88fe33d6a        $UNITY_PROJECT_PATH/Library/metadata/eb/
-#cp fail-meta/ebf102192028942cdb156ff88fe33d6a.info   $UNITY_PROJECT_PATH/Library/metadata/eb/
-#cp fail-meta/assetDatabase3                          $UNITY_PROJECT_PATH/Library/
-#cp fail-meta/guidmapper                              $UNITY_PROJECT_PATH/Library/
-#cp fail-meta/00000000000000004000000000000000        $UNITY_PROJECT_PATH/Library/metadata/00/
+echo "4.1 insert meta files which seemed to cause failure:"
+cp fail-meta/ebf102192028942cdb156ff88fe33d6a        $UNITY_PROJECT_PATH/Library/metadata/eb/
+cp fail-meta/ebf102192028942cdb156ff88fe33d6a.info   $UNITY_PROJECT_PATH/Library/metadata/eb/
+cp fail-meta/assetDatabase3                          $UNITY_PROJECT_PATH/Library/
+cp fail-meta/guidmapper                              $UNITY_PROJECT_PATH/Library/
 
 echo "4.2 save snapshot c"
 cp -r $UNITY_PROJECT_PATH snapshotc
@@ -58,11 +58,12 @@ cp -r $UNITY_PROJECT_PATH snapshotc
 echo "5. Run Unity build again ..."
 $UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -outputDir=$OUTPUT_DIR/build_step_5/
 echo "      Result: " $?
+sleep 2
 cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_5.log
 
 echo "Cleaning up: Put deleted script back"
 mv $(pwd)/tmp_deleted_file/TestScript_toDelete.cs* $UNITY_PROJECT_PATH/Assets/MyFolder/
 
-rm udsb.zip
-zip udsb.zip -r . > /dev/null
+#rm udsb.zip
+#zip udsb.zip -r . > /dev/null
 #cp udsb.zip /Volumes/Public/stuff
